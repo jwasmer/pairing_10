01909b31342b3f2d4f9c5aec544feb669c86371e
James Wasmer <jameswasmer@gmail.com>
2022-08-05 09:34:58 -0400

    Add friend if statement

--- friends.js
+++ friends.js
@@ -1,7 +1,9 @@
-var bestFriends = "Anne"
-
+var bestFriends = "Lucas"
+var bestFriends = "Katie"
 if (bestFriends === "Lucas") {
   console.log("Hi Lucas!");
+} else if (bestFriends === "Katie") {
+  console.log("Hi Katie!");
 } else {
   console.log("Hi friend!");
 }
